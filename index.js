f9a4a32a197ecff719ef5cb323085b94b13347f7
woodpecker <hans.peter.specht@gmail.com>
2025-06-19 11:01:51 +0200

    factories, constructors etc.

--- index.js
+++ index.js
@@ -12,14 +12,33 @@ function createCircle(radius) {
         }
     };
 }
+
 const circle = createCircle(1);
 
 // constructor function
 function Circle(radius) {
-    console.log('this', this);
     this.radius = radius;
     this.draw = function() {
         console.log('draw');
     }
 }
+
+// functions are objects
+const Circle1 = new Function('radius', `
+    this.radius = radius;
+    this.draw = function() {
+        console.log('draw');
+    }
+    `);
+
+const circle2 = new Circle1(1);
+
+Circle.call({}, 1);
+Circle.apply({}, [1, 2, 3]);
+
 const another = new Circle(1);
+
+// new String(); // '', "", ``
+// new Boolean(); // true, false
+// new Number(); // 1, 2, 3, ...
+
